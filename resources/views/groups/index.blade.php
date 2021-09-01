b53c93ad6fec7df6085e042e2a0e3db012630b5a
WillMeagher <willfmeagher@gmail.com>
2021-09-01 17:22:00 -0400

    Changed links for account and group to be more useful and readable.

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -7,7 +7,8 @@
             @if(count($groups) > 0)
                 @foreach($groups as $group)
                     <li class="list-group-item">
-                        <h3><a href="/group/{{str_replace(" ", "_", $group->name)}}/view"> Name: {{$group->name}} Creator: {{$group->creator_name}}</a></h3>
+                        <h3>Name: <a href="/group/{{str_replace(" ", "_", $group->name)}}/view">{{$group->name}}</a></h3>
+                        <h5>Creator: <a href="/account/{{str_replace(" ", "_", $group->creator_name)}}/view">{{$group->creator_name}}</a></h5>
                         <small>Created on {{$group->created_at->format('M d, Y')}}</small>
                     </li>
                 @endforeach
